baf61651ced6e3072d2c53e6f3e2ad8dae2ef900
Sanduni Navodya <128354320+SanduniNavodya@users.noreply.github.com>
2024-12-09 11:02:03 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Sanduni',
+    author_email = 'sanduninavodya01@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
